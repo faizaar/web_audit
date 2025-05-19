16ece9d68c4d8c4ee6b73851feee1c96ec95c1eb
faizaar <220605110001@student.uin-malang.ac.id>
2025-05-19 20:10:56 +0700

    up routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+// superadmin- Putri
 $routes->get('/', 'Home::auditee');
 
 
@@ -13,3 +14,18 @@ $routes->get('auditor', 'Home::auditor');
 
 $routes->get('superadmin', 'Home::superadmin');
 $routes->get('view/adminauditee','auditee::view_auditee');
+
+
+
+
+
+
+
+
+// Auditee- Fito
+
+
+
+
+
+// Auditor- Faiza Fina
